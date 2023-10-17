a2e2fe4104cc570c5bb2864010ed3713a6ed0561
unaivan <unaivan@gmail.com>
2023-10-17 08:41:25 +0700

    Initial Commit

---
+++ magicsaldo/magicsaldoApp.swift
@@ -0,0 +1,17 @@
+//
+//  magicsaldoApp.swift
+//  magicsaldo
+//
+//  Created by una ivan on 17/10/23.
+//
+
+import SwiftUI
+
+@main
+struct magicsaldoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
